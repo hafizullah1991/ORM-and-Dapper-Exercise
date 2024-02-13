2c7e30fc21d2f9a410e05b63d156127ca5185d5f
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-02-13 11:38:53 -0800

    done

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var instance = new DapperDepartmentRepository(conn);
+          var departmens =  instance.GetAllDepartments();
+           
+            foreach(var department in departmens)
+            {
+                Console.WriteLine(department.Name);
+                Console.WriteLine(department.DepartmentID);
+            }
         }
     }
 }
